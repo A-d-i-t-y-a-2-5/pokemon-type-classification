09052a8f605b59abcfd36b2b0284ea1cd24e3a85
A-d-i-t-y-a-2-5 <adityapratapsaha@gmail.com>
2024-11-23 11:57:28 +0530

    Confusion matrix plotting

--- training.py
+++ training.py
@@ -1,10 +1,19 @@
 from datetime import datetime
+import io
+import itertools
 import os
 import shutil
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 from tensorboard.plugins.hparams import api as hp
+from sklearn import metrics
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
 
 gpus = tf.config.list_physical_devices("GPU")
 if gpus:
@@ -41,6 +50,22 @@ class Trainer:
 
             shutil.move(src_path, dst_path)
 
+    def plot_to_image(self, figure):
+        """Converts the matplotlib plot specified by 'figure' to a PNG image and
+        returns it. The supplied figure is closed and inaccessible after this call."""
+        # Save the plot to a PNG in memory.
+        buf = io.BytesIO()
+        plt.savefig(buf, format='png')
+        # Closing the figure prevents it from being displayed directly inside
+        # the notebook.
+        plt.close(figure)
+        buf.seek(0)
+        # Convert PNG buffer to TF image
+        image = tf.image.decode_png(buf.getvalue(), channels=4)
+        # Add the batch dimension
+        image = tf.expand_dims(image, 0)
+        return image
+
     def create_dataset(self):
         self.train_ds: tf.data.Dataset = tf.keras.utils.image_dataset_from_directory(
             self.path,
@@ -73,102 +98,136 @@ class Trainer:
             batch_size=self.batch_size,
         )
 
-        class_names = self.train_ds.class_names
-        self.n_classes = len(class_names)
+        self.class_names = self.train_ds.class_names
+        # self.n_classes = len(class_names)
 
-        count = np.zeros(self.n_classes, dtype=np.int32)
-        for _, labels in self.train_ds:
-            y, _, c = tf.unique_with_counts(labels)
-            count[y.numpy()] += c.numpy()
+        # count = np.zeros(self.n_classes, dtype=np.int32)
+        # for _, labels in self.train_ds:
+        #     y, _, c = tf.unique_with_counts(labels)
+        #     count[y.numpy()] += c.numpy()
 
-        self.class_weights = {
-            i: (1 / (count.sum() - count[i])) * (count.sum() / 2.0)
-            for i in range(self.n_classes)
-        }
+        # self.class_weights = {
+        #     i: (1 / (count.sum() - count[i])) * (count.sum() / 2.0)
+        #     for i in range(self.n_classes)
+        # }
 
         AUTOTUNE = tf.data.AUTOTUNE
         self.train_ds = self.train_ds.cache().prefetch(buffer_size=AUTOTUNE)
         self.val_ds = self.val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-    def train_test_model(self, hparams):
+    def train_test_model(self, run_dir):
         # logs = "logs/" + datetime.now().strftime("%Y%m%d-%H%M%S")
 
-        # tboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logs, histogram_freq=1)
-        callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
-        model = tf.keras.Sequential(
+        tboard_callback = tf.keras.callbacks.TensorBoard(
+            log_dir=run_dir, histogram_freq=1, write_images=True
+        )
+        callback = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=10)
+        cm_callback = tf.keras.callbacks.LambdaCallback(on_epoch_end=self.log_confusion_matrix)
+
+        data_augmentation = keras.Sequential(
             [
+                layers.RandomFlip(
+                    "horizontal", input_shape=(self.img_height, self.img_width, 3)
+                ),
+                layers.RandomRotation(0.1),
+                layers.RandomZoom(0.1),
+            ]
+        )
+        self.model = tf.keras.Sequential(
+            [
+                data_augmentation,
                 tf.keras.layers.Rescaling(1.0 / 255),
-                tf.keras.layers.Conv2D(hparams[self.HP_NUM_UNITS_1], 5, activation="relu"),
+                tf.keras.layers.Conv2D(32, 5, activation="relu"),
                 tf.keras.layers.MaxPooling2D(),
-                tf.keras.layers.Conv2D(hparams[self.HP_NUM_UNITS_2], 5, activation="relu"),
+                tf.keras.layers.Conv2D(32, 5, activation="relu"),
                 tf.keras.layers.MaxPooling2D(),
-                tf.keras.layers.Conv2D(hparams[self.HP_NUM_UNITS_3], 5, activation="relu"),
+                tf.keras.layers.Conv2D(32, 5, activation="relu"),
                 tf.keras.layers.MaxPooling2D(),
+                tf.keras.layers.Dropout(0.2),
                 tf.keras.layers.Flatten(),
                 tf.keras.layers.Dense(128, activation="relu"),
                 tf.keras.layers.Dense(18, activation="softmax"),
             ]
         )
+        self.model.summary()
 
-        model.compile(
+        self.model.compile(
             loss="sparse_categorical_crossentropy",
             optimizer=tf.keras.optimizers.Adam(1e-5),
             metrics=["accuracy"],
         )
-        model.fit(
+        self.model.fit(
             self.train_ds,
-            epochs=50,
+            epochs=100,
             validation_data=self.val_ds,
-            callbacks=[callback],
-            class_weight=self.class_weights
+            callbacks=[tboard_callback, callback, cm_callback],
+            # class_weight=self.class_weights
         )
-        _, accuracy = model.evaluate(self.test_ds)
+        _, accuracy = self.model.evaluate(self.test_ds)
+
         return accuracy
 
-    def run(self, run_dir, hparams):
-        with tf.summary.create_file_writer(run_dir).as_default():
-            hp.hparams(hparams)  # record the values used in this trial
-            accuracy = self.train_test_model(hparams)
-            tf.summary.scalar("accuracy", accuracy, step=1)
+    def run(self, run_dir):
+        accuracy = self.train_test_model(run_dir)
+        print(accuracy)
 
-
-    
     def train(self):
         self.create_dataset()
-        self.HP_NUM_UNITS_1 = hp.HParam('num_units_first', hp.Discrete([16, 32, 64]))
-        self.HP_NUM_UNITS_2 = hp.HParam('num_units_second', hp.Discrete([16, 32, 64]))
-        self.HP_NUM_UNITS_3 = hp.HParam('num_units_third', hp.Discrete([16, 32, 64]))
-        with tf.summary.create_file_writer('logs/hparam_tuning').as_default():
-            hp.hparams_config(
-                hparams=[self.HP_NUM_UNITS_1, self.HP_NUM_UNITS_2, self.HP_NUM_UNITS_3],
-                metrics=[hp.Metric("accuracy", display_name='Accuracy')],
-            )
-
-        session_num = 0
-
-        for num_units_i in self.HP_NUM_UNITS_1.domain.values:
-            for num_units_j in self.HP_NUM_UNITS_2.domain.values:
-                for num_units_k in self.HP_NUM_UNITS_3.domain.values:
-                    hparams = {
-                        self.HP_NUM_UNITS_1: num_units_i,
-                        self.HP_NUM_UNITS_2: num_units_j,
-                        self.HP_NUM_UNITS_3: num_units_k,
-                        # HP_DROPOUT: dropout_rate,
-                        # HP_OPTIMIZER: optimizer,
-                    }
-                    run_name = "run-%d" % session_num
-                    print('--- Starting trial: %s' % run_name)
-                    print({h.name: hparams[h] for h in hparams})
-                    self.run('logs/hparam_tuning/' + run_name, hparams)
-                    session_num += 1
-        
-
-        
+        self.run("logs/image/" + datetime.now().strftime("%Y%m%d-%H%M%S"))
 
         # Create a TensorBoard callback
         # logs = "logs/" + datetime.now().strftime("%Y%m%d-%H%M%S")
 
         # tboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logs, histogram_freq=1)
         # callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
+    
+    def plot_confusion_matrix(self, cm, class_names):
+        """
+        Returns a matplotlib figure containing the plotted confusion matrix.
 
-        
+        Args:
+            cm (array, shape = [n, n]): a confusion matrix of integer classes
+            class_names (array, shape = [n]): String names of the integer classes
+        """
+        figure = plt.figure(figsize=(8, 8))
+        plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+        plt.title("Confusion matrix")
+        plt.colorbar()
+        tick_marks = np.arange(len(class_names))
+        plt.xticks(tick_marks, class_names, rotation=45)
+        plt.yticks(tick_marks, class_names)
+
+        # Compute the labels from the normalized confusion matrix.
+        labels = np.around(cm.astype('float') / cm.sum(axis=1)[:, np.newaxis], decimals=2)
+
+        # Use white text if squares are dark; otherwise black.
+        threshold = cm.max() / 2.
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            color = "white" if cm[i, j] > threshold else "black"
+            plt.text(j, i, labels[i, j], horizontalalignment="center", color=color)
+
+        plt.tight_layout()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+        return figure
+    
+    def log_confusion_matrix(self, epoch, logs):
+        # Use the model to predict the values from the validation dataset.
+        test_pred_raw = self.model.predict(self.test_ds)
+        test_pred = np.argmax(test_pred_raw, axis=1)
+
+        logdir = "logs/image/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+        test_labels = np.concatenate([y for x, y in self.test_ds], axis=0)
+        file_writer_cm = tf.summary.create_file_writer(logdir + '/cm')
+
+        # Calculate the confusion matrix.
+        cm = metrics.confusion_matrix(test_labels, test_pred)
+        # Log the confusion matrix as an image summary.
+        figure = self.plot_confusion_matrix(cm, class_names=self.class_names)
+        cm_image = self.plot_to_image(figure)
+
+        # Log the confusion matrix as an image summary.
+        with file_writer_cm.as_default():
+            tf.summary.image("epoch_confusion_matrix", cm_image, step=epoch)
+
+
